e07f4998886b6607ae11049baa922de07cc3301e
Patrick Hughes <patrick.hughes@coinbase.com>
2023-12-18 18:27:14 -0600

    Implement customer error handling

--- charges.go
+++ charges.go
@@ -30,6 +30,21 @@ const (
 	chargesEndpoint = "/charges"
 )
 
+type CommerceError struct {
+	ApiError    *ChargeError
+	SystemError error
+}
+
+func (e *CommerceError) Error() string {
+	if e.ApiError != nil {
+		return fmt.Sprintf("API error: %v, warnings: %v", e.ApiError.Error, e.ApiError.Warnings)
+	}
+	if e.SystemError != nil {
+		return e.SystemError.Error()
+	}
+	return "unknown error"
+}
+
 func (c *Client) setHeaders(req *http.Request) {
 	req.Header.Set("X-CC-Api-Key", c.Credentials.ApiKey)
 	req.Header.Set("X-CC-Version", "2018-03-22")
@@ -37,26 +52,26 @@ func (c *Client) setHeaders(req *http.Request) {
 	req.Header.Set("Accept", "application/json")
 }
 
-func (c *Client) CreateCharge(ctx context.Context, req *ChargeRequest) (*ChargeResponse, *ChargeError, error) {
+func (c *Client) CreateCharge(ctx context.Context, req *ChargeRequest) (*ChargeResponse, error) {
 
 	if req.LocalPrice == nil {
-		return nil, nil, errors.New("LocalPrice is required for ChargeRequest")
+		return nil, errors.New("LocalPrice is required for ChargeRequest")
 	}
 
 	if req.PricingType == "" {
-		return nil, nil, errors.New("PricingType is required for ChargeRequest")
+		return nil, errors.New("PricingType is required for ChargeRequest")
 	}
 
 	url := fmt.Sprintf("%s%s", c.HttpBaseUrl, chargesEndpoint)
 
 	payload, err := json.Marshal(req)
 	if err != nil {
-		return nil, nil, err
+		return nil, err
 	}
 
 	httpReq, err := http.NewRequestWithContext(ctx, http.MethodPost, url, bytes.NewBuffer(payload))
 	if err != nil {
-		return nil, nil, err
+		return nil, err
 	}
 
 	c.setHeaders(httpReq)
@@ -64,43 +79,42 @@ func (c *Client) CreateCharge(ctx context.Context, req *ChargeRequest) (*ChargeR
 	resp, err := c.HttpClient.Do(httpReq)
 
 	if err != nil {
-		return nil, nil, err
+		return nil, err
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode < 200 || resp.StatusCode >= 300 {
 		chargeErr, err := handleErrorResponse(resp)
 		if err != nil {
-			fmt.Printf("error: %s", err)
-			return nil, nil, err
+			return nil, &CommerceError{SystemError: err}
 		}
-		return nil, chargeErr, nil
+		return nil, &CommerceError{ApiError: chargeErr}
 	}
 
 	body, err := io.ReadAll(resp.Body)
 
 	var chargeResponse *ChargeResponse
 	if err = json.Unmarshal(body, &chargeResponse); err != nil {
-		return nil, nil, err
+		return nil, err
 	}
 
-	return chargeResponse, nil, nil
+	return chargeResponse, nil
 }
 
-func (c *Client) GetCharge(ctx context.Context, chargeId string) (*ChargeResponse, *ChargeError, error) {
+func (c *Client) GetCharge(ctx context.Context, chargeId string) (*ChargeResponse, error) {
 
 	url := fmt.Sprintf("%s%s/%s", c.HttpBaseUrl, chargesEndpoint, chargeId)
 
 	httpReq, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
 	if err != nil {
-		return nil, nil, err
+		return nil, err
 	}
 
 	c.setHeaders(httpReq)
 
 	resp, err := c.HttpClient.Do(httpReq)
 	if err != nil {
-		return nil, nil, err
+		return nil, err
 	}
 
 	defer resp.Body.Close()
@@ -108,20 +122,19 @@ func (c *Client) GetCharge(ctx context.Context, chargeId string) (*ChargeRespons
 	if resp.StatusCode < 200 || resp.StatusCode >= 300 {
 		chargeErr, err := handleErrorResponse(resp)
 		if err != nil {
-			fmt.Printf("error: %s", err)
-			return nil, nil, err
+			return nil, &CommerceError{SystemError: err}
 		}
-		return nil, chargeErr, nil
+		return nil, &CommerceError{ApiError: chargeErr}
 	}
 
 	body, err := io.ReadAll(resp.Body)
 
 	var chargeResponse *ChargeResponse
 	if err := json.Unmarshal(body, &chargeResponse); err != nil {
-		return nil, nil, err
+		return nil, err
 	}
 
-	return chargeResponse, nil, nil
+	return chargeResponse, nil
 
 }
 
@@ -133,7 +146,6 @@ func handleErrorResponse(response *http.Response) (*ChargeError, error) {
 		return nil, err
 	}
 	if err := json.Unmarshal(body, &chargeErr); err != nil {
-		log.Fatalf("error: %s", err)
 		return nil, err
 	}
 	return chargeErr, nil
